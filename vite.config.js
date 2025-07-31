8fff3906ed9a49c3b25c5cb7aa640c7a36993db0
ElArpan <b.carrasco55@alumnos.santotomas.cl>
2025-07-30 23:08:58 -0400

    Configurar vite.root para index.html en la raíz

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  root: '.',          // Asegura que Vite use la carpeta raíz
+  publicDir: 'public',// Tus assets siguen en public/
+  build: {
+    outDir: 'dist',   // Carpeta de salida estándar
+    emptyOutDir: true,
+  },
   plugins: [react()],
-  server: {
-    historyApiFallback: true
-  }
 })
